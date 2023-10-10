316852d8e5007a1d454c1958ff65dd17847d7c4d
AmitMali <amit23289@gmail.com>
2023-10-10 13:35:52 +0530

    javascript conditionals

---
+++ javascript/conditionals.js
@@ -0,0 +1,57 @@
+var no = 10;
+
+// if(no>100)
+// {
+//     console.log("grater than 100")
+// }
+// else
+// {
+//     console.log("less than 100")
+// }
+
+// if (no % 2 == 0) {
+//   console.log("Even");
+// } else {
+//   console.log("odd");
+// }
+
+// var no1 = 20,
+//   no2 = 34,
+//   no3 = 23;
+// if (no1 > no2 && no1 > no3) {
+//   console.log(no1 + " is greater");
+// } else if (no2 > no1 && no2 > no3) {
+//   console.log(no2 + " is greater");
+// } else {
+//   console.log(no3 + " is greater");
+// }
+
+var choice = 8;
+switch (choice) {
+  case 1:
+    console.log("Monday");
+    break;
+
+  case 2:
+    console.log("Tuesday");
+    break;
+
+  case 3:
+    console.log("Wednesday");
+    break;
+
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("invalid choice");
+}
