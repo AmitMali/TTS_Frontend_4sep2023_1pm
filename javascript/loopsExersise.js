5cb7a13ae3df4a9e5e4d86ccb99f3802ba0cf661
AmitMali <amit23289@gmail.com>
2023-10-11 13:44:16 +0530

    loops exersise

--- javascript/loopsExersise.js
+++ javascript/loopsExersise.js
@@ -19,3 +19,32 @@
 // console.log(fact);
 
 // find all evan and odd numbers in range between 1to 50
+
+// console.log("even numbers");
+// for (let n = 1; n <= 50; n++) {
+//   if (n % 2 == 0) {
+//     console.log(n);
+//   }
+// }
+// console.log("odd numbers");
+// for (let n = 1; n <= 50; n++) {
+//   if (n % 2 != 0) {
+//     console.log(n);
+//   }
+// }
+
+// program to check prime number
+
+// num = 9;
+// isPrime = true;
+// for (let i = 2; i < num; i++) {
+//   if (num % i == 0) {
+//     isPrime = false;
+//     break;
+//   }
+// }
+// if (isPrime) {
+//   console.log(num + " is a prime number");
+// } else {
+//   console.log(num + " is not a prime number");
+// }
