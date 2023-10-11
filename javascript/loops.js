85f5fefcef7a315b049c5664e27ec6d19edf7b4e
AmitMali <amit23289@gmail.com>
2023-10-11 13:19:58 +0530

    loops

---
+++ javascript/loops.js
@@ -0,0 +1,14 @@
+// no = 11;
+// while (no <= 10) {
+//   console.log("hello ");
+//   no = no + 1;
+// }
+
+// do {
+//   console.log(no);
+//   no = no + 1;
+// } while (no <= 10);
+
+// for (let no = 1; no <= 10; no = no + 1) {
+//   console.log(no);
+// }
---
+++ javascript/loopsExersise.js
@@ -0,0 +1,21 @@
+// program to find total from 1 to 10
+
+// no = 1;
+// total = 0;
+// while (no <= 10) {
+//   total = total + no;
+//   no += 1;
+// }
+// console.log(total);
+
+// Program to create factorial
+
+// no = 1;
+// fact = 1;
+// while (no <= 5) {
+//   fact = fact * no;
+//   no += 1;
+// }
+// console.log(fact);
+
+// find all evan and odd numbers in range between 1to 50
